b6c8ea5643dd5579b503f6b69cd63da1d28b9819
gure-san <azrarafani@gmail.com>
2021-09-20 21:55:29 +0700

    megatasi bug pertama 5x dan menambahkan beberapa fitur

--- js/home.js
+++ js/home.js
@@ -9,26 +9,24 @@ tombol.addEventListener('click', function() {
 // detail tiket promo
 const detailBase = document.querySelectorAll('.detail');
 const containerButton = document.querySelector('main');
-const boxAni = document.querySelectorAll('.box');
-// console.log();
-let i = 0;
+const boxani = Array.from(document.querySelectorAll('.box'));
 
 containerButton.addEventListener('click', e => {
         if(e.target.classList.contains('tombol-satu')) {
                 detailBase[0].classList.add('fade-out');
-                boxAni[0].classList.add('boxAni');
+                boxani[0].classList.add('boxAni');
                 return e;
         }
         
         if(e.target.classList.contains('tombol-dua')) {
                 detailBase[1].classList.add('fade-out');
-                boxAni[1].classList.add('boxAni');
+                boxani[1].classList.add('boxAni');
                 return e;
         }
 
         if(e.target.classList.contains('tombol-tiga')) {
                 detailBase[2].classList.add('fade-out');
-                boxAni[2].classList.add('boxAni');
+                boxani[2].classList.add('boxAni');
                 return e;
         }
 
@@ -37,6 +35,47 @@ containerButton.addEventListener('click', e => {
                 e.target.parentNode.classList.remove('boxAni');
                 return e;
         }
+
+        return e; 
+});
+
+
+// detail layanan
+const cardLayanan = document.querySelectorAll('.detailLayanan');
+const layanan = document.querySelector('section');
+const boxaniCard = Array.from(document.querySelectorAll('.bflex'));
+
+layanan.addEventListener('click', e => {
+        if(e.target.classList.contains('tombol-satu')) {
+                cardLayanan[0].classList.add('fade-out');
+                boxaniCard[0].classList.add('boxAni');
+                return e;
+        }
+        
+        if(e.target.classList.contains('tombol-dua')) {
+                cardLayanan[1].classList.add('fade-out');
+                boxaniCard[1].classList.add('boxAni');
+                return e;
+        }
+
+        if(e.target.classList.contains('tombol-tiga')) {
+                cardLayanan[2].classList.add('fade-out');
+                boxaniCard[2].classList.add('boxAni');
+                return e;
+        }
+
+        if(e.target.classList.contains('tombol-empat')) {
+                cardLayanan[3].classList.add('fade-out');
+                boxaniCard[3].classList.add('boxAni');
+                return e;
+        }
+
+        if(e.target.classList.contains('close')) {
+                e.target.parentNode.parentNode.classList.remove('fade-out');
+                e.target.parentNode.classList.remove('boxAni');
+                return e;
+        }
+
         return e; 
 });
 
