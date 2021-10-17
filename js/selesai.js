28a981a311ddabf76d225e976b47a31a07607b99
gure-san <azrarafani@gmail.com>
2021-10-17 09:55:58 +0700

    membuat halaman proses pembayaran tiket

---
+++ js/selesai.js
@@ -0,0 +1,5 @@
+const generateLink = document.querySelector('.generate');
+const link = document.querySelector('.link-generate');
+generateLink.addEventListener('click', function() {
+    link.classList.add('ubah');
+})
